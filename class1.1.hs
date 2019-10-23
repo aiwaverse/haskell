1495d0fa883969b76cd2df70042d34dece60ed36
aiwa <aiwavision@protonmail.com>
2019-10-23 19:54:39 -0300

    formatted

--- class1.1.hs
+++ class1.1.hs
@@ -1,21 +1,19 @@
 toDigits :: Integer -> [Integer]
-toDigits num = 
-    reverse (toDigitsRev num)
+toDigits num = reverse (toDigitsRev num)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev num
-    | num <= 0  = []
-    | otherwise = (num `mod` 10) : toDigitsRev (num `div` 10)
+toDigitsRev num | num <= 0  = []
+                | otherwise = (num `mod` 10) : toDigitsRev (num `div` 10)
 
-    
+
 {- makes an indexed list out an infnite list and the reverse list, decides if double or
     not, then re-reverse the list-}
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther l = reverse [doubleOrNot x i | (i,x) <- zip [0..] rl]
+doubleEveryOther l = reverse [ doubleOrNot x i | (i, x) <- zip [0 ..] rl ]
     where rl = reverse l
 
 doubleOrNot :: Integer -> Integer -> Integer
-doubleOrNot n i = if odd i then n*2 else n
+doubleOrNot n i = if odd i then n * 2 else n
 
 -- combining results
 sumDigits :: [Integer] -> Integer
@@ -27,13 +25,12 @@ correctDigits = map sumBreak
 
 -- sum digits of a number with n digits
 sumBreak :: Integer -> Integer
-sumBreak n
-    | n < 10 = n
-    | otherwise = sum (toDigits n)
+sumBreak n | n < 10    = n
+           | otherwise = sum (toDigits n)
 
 validate :: Integer -> Bool
-validate card
-    | sumDigits doubleCardDigits `mod` 10 == 0 = True
-    | otherwise                                = False
-    where cardDigits = toDigits card
-          doubleCardDigits = doubleEveryOther cardDigits
+validate card | sumDigits doubleCardDigits `mod` 10 == 0 = True
+              | otherwise = False
+  where
+    cardDigits       = toDigits card
+    doubleCardDigits = doubleEveryOther cardDigits
--- class1.2.hs
+++ class1.2.hs
@@ -3,9 +3,15 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi discs from to aux =
-    hanoi (discs - 1) from aux to ++ 
-    [(from, to)] ++ 
-    hanoi (discs - 1) aux to from
+    hanoi (discs - 1) from aux to
+        ++ [(from, to)]
+        ++ hanoi (discs - 1) aux to from
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 0 _ _ _ _ = []
-hanoi4 n from to aux1 aux2 = undefined
+hanoi4 (-1) _ _ _ _ = []
+hanoi4 0    _ _ _ _ = []
+hanoi4 n from to aux1 aux2 =
+    hanoi4 (n - 2) from aux1 aux2 to
+        ++ [(from, aux2)]
+        ++ [(from, to)]
+        ++ [(aux2, to)]
+        ++ hanoi4 (n - 2) aux1 to from aux2
