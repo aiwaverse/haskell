706da238db95a88425e7ab772e51f459ce7f165a
Agatha <aiwavision@protonmail.com>
2019-10-31 17:47:23 -0300

    broke

--- robot.hs
+++ robot.hs
@@ -7,7 +7,7 @@ name (n, _, _) = n
 attack :: (String, Integer, Integer) -> Integer
 attack (_, a, _) = a
 
-hp :: (String, Integer, Integer) -> Integer
+--hp :: (String, Integer, Integer) -> Integer
 hp (_, _, hp) = hp
 
 getName :: (((String, Integer, Integer) -> String) -> String) -> String
@@ -16,7 +16,7 @@ getName aRobot = aRobot name
 getAttack :: (((String, Integer, Integer) -> Integer) -> Integer) -> Integer
 getAttack aRobot = aRobot attack
 
-getHp :: (((String, Integer, Integer) -> Integer) -> Integer) -> Integer
+--getHp :: (((String, Integer, Integer) -> Integer) -> Integer) -> Integer
 getHp aRobot = aRobot hp
 
 geeRobot :: ((String, Integer, Integer) -> t) -> t
@@ -34,14 +34,46 @@ gigiRobot = robot ("Gigi", 25, 50)
 louieRobot :: ((String, Integer, Integer) -> t) -> t
 louieRobot = robot ("Louie", 12, 90)
 
-setName :: (((String, Integer, Integer) -> ((String, Integer, Integer) -> t1) -> t1) -> t2) -> String -> t2
+setName
+    :: (  (  (String, Integer, Integer)
+         -> ((String, Integer, Integer) -> t1)
+         -> t1
+         )
+       -> t2
+       )
+    -> String
+    -> t2
 setName aRobot newName = aRobot (\(n, a, h) -> robot (newName, a, h))
 
-setAttack :: (((String, Integer, Integer) -> ((String, Integer, Integer) -> t1) -> t1) -> t2) -> Integer -> t2
+setAttack
+    :: (  (  (String, Integer, Integer)
+         -> ((String, Integer, Integer) -> t1)
+         -> t1
+         )
+       -> t2
+       )
+    -> Integer
+    -> t2
 setAttack aRobot newAttack = aRobot (\(n, a, h) -> robot (n, newAttack, h))
 
-setHp :: (((String, Integer, Integer) -> ((String, Integer, Integer) -> t1) -> t1) -> t2) -> Integer -> t2
+setHp
+    :: (  (  (String, Integer, Integer)
+         -> ((String, Integer, Integer) -> t1)
+         -> t1
+         )
+       -> t2
+       )
+    -> Integer
+    -> t2
 setHp aRobot newHp = aRobot (\(n, a, h) -> robot (n, a, newHp))
 
 printRobot :: (((String, Integer, Integer) -> String) -> String) -> String
-printRobot aRobot = aRobot (\(n,a,h) -> "Name: " ++ n ++ " ,Attack: " ++ show a ++ " ,HP: " ++ show h)
+printRobot aRobot = aRobot
+    (\(n, a, h) -> "Name: " ++ n ++ " ,Attack: " ++ show a ++ " ,HP: " ++ show h
+    )
+
+damage aRobot damageDealt = setHp aRobot (remainingHealth (getHp aRobot) damageDealt)
+
+remainingHealth :: Integer -> Integer -> Integer
+remainingHealth health damageDealt | damageDealt >= health = 0
+                                   | otherwise = health - damageDealt
