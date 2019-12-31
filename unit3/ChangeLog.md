48b799cab5b6544cb41b2d6a566d7109b80311a2
aiwa <aiwavision@protonmail.com>
2019-12-31 09:19:19 -0300

    started unit3

---
+++ unit3/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for unit3
+
+## Unreleased changes
---
+++ unit3/src/Store.hs
@@ -0,0 +1,24 @@
+module Store where
+
+type FirstName = String
+
+type LastName = String
+
+type MiddleName = String
+
+data Name
+   = Name FirstName LastName
+   | NameWithMiddle FirstName MiddleName LastName
+   | TwoInitialsWithLast Char Char LastName deriving (Show)
+
+data Creator = AuthorCreator Author | ArtistCreator Artist deriving (Show)
+
+newtype Author = Author Name
+
+instance Show Author where 
+   show (Author n) = show n
+
+data Artist = Person Name | Band String deriving (Show)
+
+hpLovecraft :: Creator 
+hpLovecraft = AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
