b2d21cbf7bb0c3bfddbf14da8985d31b30a1f35a
aiwa <aiwavision@protonmail.com>
2019-12-31 12:36:57 -0300

    have to dwell on formatters

--- unit3/src/Store.hs
+++ unit3/src/Store.hs
@@ -9,16 +9,24 @@ type MiddleName = String
 data Name
    = Name FirstName LastName
    | NameWithMiddle FirstName MiddleName LastName
-   | TwoInitialsWithLast Char Char LastName deriving (Show)
+   | TwoInitialsWithLast Char Char LastName
+   deriving (Show)
 
-data Creator = AuthorCreator Author | ArtistCreator Artist deriving (Show)
+data Creator
+   = AuthorCreator Author
+   | ArtistCreator Artist
+   deriving (Show)
 
-newtype Author = Author Name
+newtype Author =
+   Author Name
 
-instance Show Author where 
+instance Show Author where
    show (Author n) = show n
 
-data Artist = Person Name | Band String deriving (Show)
+data Artist
+   = Person Name
+   | Band String
+   deriving (Show)
 
-hpLovecraft :: Creator 
+hpLovecraft :: Creator
 hpLovecraft = AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
