267921d6046f9ccae9056f6b987fa47aa07c62c6
aiwa <aiwavision@protonmail.com>
2020-01-05 09:58:12 -0300

    added identity color

--- unit3/src/Lesson17.hs
+++ unit3/src/Lesson17.hs
@@ -10,21 +10,27 @@ data Color
   | Purple
   | Orange
   | Brown
+  | Transparent
   deriving (Show,Eq)
 
 instance Semigroup Color where
-  (<>) Red    Blue   = Purple
-  (<>) Blue   Red    = Purple
-  (<>) Yellow Blue   = Green
-  (<>) Blue   Yellow = Green
-  (<>) Yellow Red    = Orange
-  (<>) Red    Yellow = Orange
+  (<>) Transparent a           = a
+  (<>) a           Transparent = a
+  (<>) Red         Blue        = Purple
+  (<>) Blue        Red         = Purple
+  (<>) Yellow      Blue        = Green
+  (<>) Blue        Yellow      = Green
+  (<>) Yellow      Red         = Orange
+  (<>) Red         Yellow      = Orange
   (<>) a b | a == b    = a
            | all (`elem` [Red, Blue, Purple]) [a, b] = Purple
            | all (`elem` [Yellow, Blue, Green]) [a, b] = Green
            | all (`elem` [Red, Yellow, Orange]) [a, b] = Orange
            | otherwise = Brown
 
+instance Monoid Color where
+  mempty = Transparent
+  mappend c1 c2 = c1 <> c2
 
 type Events = [String]
 type Probs = [Double]
