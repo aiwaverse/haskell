46dfbde6a6490e5a321754e64c58793f00726d86
aiwa <aiwavision@protonmail.com>
2020-01-05 00:04:45 -0300

    finished lesson17 (no exercises)

--- unit3/src/Lesson17.hs
+++ unit3/src/Lesson17.hs
@@ -2,7 +2,6 @@ module Lesson17 where
 
 import           Data.List
 import           Data.Semigroup
-
 data Color
   = Red
   | Yellow
@@ -25,3 +24,66 @@ instance Semigroup Color where
            | all (`elem` [Yellow, Blue, Green]) [a, b] = Green
            | all (`elem` [Red, Yellow, Orange]) [a, b] = Orange
            | otherwise = Brown
+
+
+type Events = [String]
+type Probs = [Double]
+
+data PTable = PTable Events Probs
+
+createPTable :: Events -> Probs -> PTable
+createPTable events probs = PTable events normalizedProbs
+ where
+  totalProbs      = sum probs
+  normalizedProbs = map (/ totalProbs) probs
+
+showPair :: String -> Double -> String
+showPair event prob = mconcat [event, "|", show prob, "\n"]
+
+instance Show PTable where
+  show (PTable events probs) = removeLast . mconcat $ pairs
+    where pairs = zipWith showPair events probs
+
+removeLast :: (Ord a) => [a] -> [a]
+removeLast lst = take almostAll lst where almostAll = length lst - 1
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+ where
+  nToAdd     = length l2
+  repeatedL1 = map (replicate nToAdd) l1
+  newL1      = mconcat repeatedL1
+  cycledL2   = cycle l2
+
+myProduct :: (a -> b -> c) -> [a] -> [b] -> [c]
+myProduct func l1 l2 = map funcToPairs productList
+ where
+  productList = [ (x, y) | x <- l1, y <- l2 ]
+  funcToPairs = uncurry func
+
+
+combineEvents :: Events -> Events -> Events
+combineEvents = myProduct combiner where combiner x y = mconcat [x, "-", y]
+
+combineProbs :: Probs -> Probs -> Probs
+combineProbs = myProduct (*)
+
+instance Semigroup PTable where
+  (<>) ptable1        (PTable [] []) = ptable1
+  (<>) (PTable [] []) ptable2        = ptable2
+  (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
+   where
+    newEvents = combineEvents e1 e2
+    newProbs  = combineProbs p1 p2
+
+instance Monoid PTable where
+  mempty = PTable [] []
+  mappend pt1 pt2 = pt1 <> pt2
+
+coin :: PTable
+coin = createPTable ["heads", "tails"] [0.5, 0.5]
+
+spinner :: PTable
+spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
+
+
