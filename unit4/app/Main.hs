073b398332f58c0f0f21376d655c3abf41df6b8d
aiwa <aiwavision@protonmail.com>
2020-01-06 11:50:12 -0300

    did the pizza thing

--- unit4/app/Main.hs
+++ unit4/app/Main.hs
@@ -1,5 +1,22 @@
 module Main where
 
+import Lesson21
+
+pizzaFunction :: IO ()
+pizzaFunction = do
+  putStrLn "What is the size of pizza 1"
+  size1 <- getLine
+  putStrLn "What is the cost of pizza 1"
+  cost1 <- getLine
+  putStrLn "What is the size of pizza 2"
+  size2 <- getLine
+  putStrLn "What is the cost of pizza 2"
+  cost2 <- getLine
+  let pizza1 = Pizza (read size1) (read cost1)
+  let pizza2 = Pizza (read size2) (read cost2)
+  let betterPizza = comparePizzas pizza1 pizza2
+  putStrLn (describePizza betterPizza)
 
 main :: IO ()
-main = undefined
+main = pizzaFunction
+
--- unit4/src/Lesson21.hs
+++ unit4/src/Lesson21.hs
@@ -1,5 +1,27 @@
-module Lesson12 where
+module Lesson21 where
 
 data Pizza = Pizza { size :: Double
                    , cost :: Double
                    }
+
+costPerCm :: Pizza -> Double
+costPerCm p = cost p / (areaGivenDiameter . size $ p)
+
+areaGivenDiameter :: Double -> Double
+areaGivenDiameter diam = pi * radius ^ 2 where radius = diam / 2
+
+comparePizzas :: Pizza -> Pizza -> Pizza
+comparePizzas p1 p2 | costP1 < costP2 = p1
+                    | otherwise       = p2
+  where
+    costP1 = costPerCm p1
+    costP2 = costPerCm p2
+
+describePizza :: Pizza -> String
+describePizza pizza@(Pizza s c) = mconcat
+  [ "The "
+  , show s
+  , " pizza is cheaper at "
+  , show $ costPerCm pizza
+  , " per square cm"
+  ]
