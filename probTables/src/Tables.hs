2def73d9c9baaa42a4ce81daea2169139bc016bd
aiwa <aiwavision@protonmail.com>
2020-04-08 00:34:23 -0300

    i was testing themes and decided to fix warnings

--- probTables/src/Tables.hs
+++ probTables/src/Tables.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
 module Tables
   ( ProbData()
   , ProbElement()
@@ -17,61 +18,6 @@ import           Data.List
 import qualified GHC.Generics                  as G
 import           Ranges
 import           Text.PrettyPrint.Tabulate     as T
-
-
-rawData :: [Double]
-rawData =
-  [ 3.11
-  , 8.88
-  , 9.26
-  , 10.81
-  , 12.69
-  , 13.78
-  , 15.23
-  , 15.62
-  , 17.00
-  , 17.39
-  , 18.36
-  , 18.43
-  , 19.27
-  , 19.50
-  , 19.54
-  , 20.16
-  , 20.59
-  , 22.22
-  , 23.04
-  , 24.47
-  , 24.58
-  , 25.13
-  , 26.24
-  , 26.26
-  , 27.65
-  , 28.06
-  , 28.08
-  , 28.38
-  , 32.03
-  , 36.37
-  , 38.98
-  , 38.64
-  , 39.16
-  , 41.02
-  , 42.97
-  , 44.08
-  , 44.67
-  , 45.40
-  , 46.69
-  , 48.65
-  , 50.39
-  , 52.75
-  , 54.80
-  , 59.07
-  , 61.22
-  , 70.32
-  , 82.70
-  , 85.76
-  , 86.37
-  , 93.34
-  ]
 
 data ProbData = ProbData { n      :: Integer
                          , k      :: Integer
@@ -102,24 +48,23 @@ data ProbTable = ProbTable { j                  :: [Integer]
 
 
 buildData :: [Double] -> ProbData
-buildData values = ProbData n k roundedI sortedValues
+buildData vals = ProbData n' k' roundedI sortedValues
   where
-    sortedValues = sort values
-    n            = genericLength values
-    k            = round $ 1 + 3.32 * (logBase 10 . fromIntegral $ n)
-    i            = (/ fromIntegral k) $ last sortedValues - head sortedValues
-    roundedI     = roundTo i 1
+    sortedValues = sort vals
+    n'            = genericLength vals
+    k'            = round $ 1 + 3.32 * (logBase @Double 10 . fromIntegral $ n')
+    i'            = (/ fromIntegral k') $ last sortedValues - head sortedValues
+    roundedI     = roundTo i' 1
 
 
 buildTable :: ProbData -> ProbTable
-buildTable pdata = ProbTable j r af af' rf rf' cc
+buildTable pdata = ProbTable j' r af af' rf rf' cc
   where
-    j           = [1 .. k pdata]
-    firstValue  = head . values $ pdata
+    j'           = [1 .. k pdata]
     tableRanges = createRange (values pdata) (i pdata) (k pdata)
     r           = rangeString tableRanges
     af =
-      map (\range -> genericLength $ values pdata `inRange` range) tableRanges
+      map (\ran -> genericLength $ values pdata `inRange` ran) tableRanges
     af' = scanl1 (+) af
     rf  = map ((/ (fromIntegral $ n pdata)) . fromIntegral) af  --Thanks hlint
     rf' = scanl1 (+) rf
@@ -129,9 +74,6 @@ buildTable pdata = ProbTable j r af af' rf rf' cc
 -- I only discovered too late I needed this, so this is my solution to not rewrite
 tableToList :: ProbTable -> [ProbElement]
 tableToList (ProbTable [] _ _ _ _ _ _) = []
-tableToList (ProbTable (j : js) (r : rs) (af : afs) (af' : afs') (rf : rfs) (rf' : rfs') (cc : ccs))
-  = ProbElement j r af af' rf rf' cc
-    : tableToList (ProbTable js rs afs afs' rfs rfs' ccs)
-
-fullTable :: [ProbElement]
-fullTable = tableToList . buildTable . buildData $ rawData
+tableToList (ProbTable (jx : jxs) (r : rs) (af : afs) (af' : afs') (rf : rfs) (rf' : rfs') (cc : ccs))
+  = ProbElement jx r af af' rf rf' cc
+    : tableToList (ProbTable jxs rs afs afs' rfs rfs' ccs)
