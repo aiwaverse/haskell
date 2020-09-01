d67556f334ea9945e75e814699849bb98267f643
aiwaverse <aiwavision@protonmail.com>
2020-09-01 12:12:23 -0300

    adjusted typing to be better, fixed typo

--- numeric-class/src/Roots/Bissection.hs
+++ numeric-class/src/Roots/Bissection.hs
@@ -1,13 +1,15 @@
-module Roots.Bissection (bissection) where
+module Roots.Bissection
+    ( bissection
+    )
+where
 
 -- | Calculates the root of a function @f@ given two extremes @a@ and @b@
 -- a @tolerance and an "iteration" limit @n (it's recursive)
-bissection :: (Num a1, Ord t, Ord a1, Fractional t) => (t -> a1) -> t -> t -> t -> Int -> t
-bissection _ _ _ _ 0 = error "Number of iterations exceed"
-bissection f a b tolerance n
-  | f p == 0 = p
-  | (b - a) / 2 < tolerance = p
-  | (f a * f p) > 0 = bissection f p b tolerance (n - 1)
-  | otherwise = bissection f a p tolerance (n - 1)
-  where
-    p = a + (b - a) / 2
+
+bissection :: (Double -> Double) -> Double -> Double -> Double -> Int -> Double
+bissection _ _ _ _ 0 = error "Number of iterations exceeded"
+bissection f a b tolerance n | f p == 0 = p
+                             | (b - a) / 2 < tolerance = p
+                             | (f a * f p) > 0 = bissection f p b tolerance (n - 1)
+                             | otherwise = bissection f a p tolerance (n - 1)
+    where p = a + (b - a) / 2
