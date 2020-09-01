df12578de042bb1d5f39c0534d7b622410efe22b
aiwaverse <aiwavision@protonmail.com>
2020-09-01 12:36:59 -0300

    i keep changing the format

--- numeric-class/src/Roots/Bissection.hs
+++ numeric-class/src/Roots/Bissection.hs
@@ -5,11 +5,10 @@ where
 
 -- | Calculates the root of a function @f@ given two extremes @a@ and @b@
 -- a @tolerance and an "iteration" limit @n (it's recursive)
-
 bissection :: (Double -> Double) -> Double -> Double -> Double -> Int -> Double
 bissection _ _ _ _ 0 = error "Number of iterations exceeded"
-bissection f a b tolerance n | f p == 0 = p
-                             | (b - a) / 2 < tolerance = p
-                             | (f a * f p) > 0 = bissection f p b tolerance (n - 1)
+bissection f a b tolerance n | f p == 0 = p                                          -- a perfect root
+                             | (b - a) / 2 < tolerance = p                           -- the difference between the two extremes is low enough to call it "tolerable"
+                             | (f a * f p) > 0 = bissection f p b tolerance (n - 1)  --simply tests if p will replace a or b
                              | otherwise = bissection f a p tolerance (n - 1)
     where p = a + (b - a) / 2
