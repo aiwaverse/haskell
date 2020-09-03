d5055bd303c2b7c59ae6195eaf8fe29cec2dda7a
aiwaverse <aiwavision@protonmail.com>
2020-09-03 15:05:35 -0300

    changed the strategy to the base-noprelude

--- probFormulas/src/Area1.hs
+++ probFormulas/src/Area1.hs
@@ -1,6 +1,19 @@
-module Area1(combinations) where
+module Area1
+  ( combinations
+  )
+where
 
-import Data.List
+import           Data.List               hiding ( head )
 
 combinations :: Int -> [a] -> [[a]]
 combinations n xs = filter (\x -> n == length x) $ subsequences xs
+
+fiveNumbers :: (Ord a, Num a) => [a] -> (Maybe a, Maybe a, Maybe a, Maybe a, Maybe a)
+fiveNumbers xs = undefined
+ where
+  sortedList = sort xs
+  listSize   = genericLength sortedList
+  ei         = viaNonEmpty head xs
+  q1         = sortedList !!? (round . (subtract 1) $ (listSize + 1) / 4)
+  md         = sortedList !!? (round . (subtract 1) $ (listSize + 1) / 4)
+
