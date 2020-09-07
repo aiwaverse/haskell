0f28aed9a4377aa707e4eaacbcceb42f602d3f7a
aiwaverse <aiwavision@protonmail.com>
2020-09-07 12:30:51 -0300

    Changed the Prelude.hs to include log

--- numeric-class/src/Prelude.hs
+++ numeric-class/src/Prelude.hs
@@ -1,5 +1,18 @@
 module Prelude
   ( module Relude
+  , module Relude.Extra
+  , module Relude.Numeric
+  , log
+  , euler
   )
 where
 import           Relude
+import           Relude.Extra
+
+import           Relude.Numeric
+
+euler :: Double
+euler = exp 1
+
+log :: Double -> Double
+log = logBase euler
--- numeric-class/src/Roots/FixedPoint.hs
+++ numeric-class/src/Roots/FixedPoint.hs
@@ -3,6 +3,8 @@ module Roots.FixedPoint
   )
 where
 
+-- | Given a function @f@, an @estimate@ and the maximum @iteractions@
+-- Returns an approximate point where f(estimate) = estimate
 fixedPoint :: (Double -> Double) -> Double -> Int -> Double
 fixedPoint _ estimate 0           = estimate
 fixedPoint f estimate iteractions = fixedPoint f (f estimate) (iteractions - 1)
