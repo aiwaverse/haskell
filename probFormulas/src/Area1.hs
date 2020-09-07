7ffb5a04d0bda2ea93e61f73a846348d84c53ace
aiwaverse <aiwavision@protonmail.com>
2020-09-06 23:08:08 -0300

    Area1

--- probFormulas/src/Area1.hs
+++ probFormulas/src/Area1.hs
@@ -10,15 +10,25 @@ module Area1
   , median
   , firstQuartile
   , thirdQuartile
+  , tanques
   )
 where
-
 
 import           Data.List               hiding ( head
                                                 , last
                                                 )
 import           Relude
 import           Relude.Unsafe                  ( fromJust )
+
+
+tanques1 = [189, 220, 227, 231, 239, 257, 269, 290]
+tanques2 = [195, 220, 229, 231, 249, 258, 269, 290]
+tanques3 = [214, 222, 229, 232, 253, 259, 270, 313]
+tanques4 = [218, 223, 230, 232, 253, 260, 274, 361]
+tanques5 = [220, 224, 231, 237, 254, 268, 277, 375]
+tanques :: [Double]
+tanques = mconcat [tanques1, tanques2, tanques3, tanques4, tanques5]
+
 
 -- | given the size of the combinations and a list, produces all the possible subsequences of that size
 combinations :: Int -> [a] -> [[a]]
@@ -46,28 +56,35 @@ fiveNumbers xs = (ei, q1, md, q3, es)
   es         = viaNonEmpty last sortedList
 
 firstQuartile :: (Ord a, Fractional a) => [a] -> Maybe a
-firstQuartile (sort -> xs) = if odd listSize
-  then xs !!? (listSize `div` 4)
-  else liftA2 (\x y -> (x + y) / 2)
-              (xs !!? (listSize `div` 4))
-              (xs !!? ((listSize `div` 4) - 1))
-  where listSize = length xs + 1
+firstQuartile (sort -> xs) = if odd (length xs)
+  then mean (xs !!? ceiling pOdd, xs !!? floor pOdd)
+  else mean (xs !!? ceiling pEven, xs !!? floor pEven)
+  where
+    pOdd = ((genericLength xs + 1) / 4) - 1
+    pEven = ((genericLength xs + 2) / 4) - 1
+    mean (Just x, Just y) = Just ((x + y) / 2)
+    mean (Nothing, _) = Nothing
+    mean (_, Nothing) = Nothing
+
 
 thirdQuartile :: (Ord a, Fractional a) => [a] -> Maybe a
-thirdQuartile (sort -> xs) = if odd listSize
-  then xs !!? (3 * (listSize `div` 4))
-  else liftA2 (\x y -> (x + y) / 2)
-              (xs !!? (3 * listSize `div` 4))
-              (xs !!? ((3 * listSize `div` 4) - 1))
-  where listSize = length xs + 1
+thirdQuartile (sort -> xs) = if odd (length xs)
+  then mean (xs !!? ceiling pOdd, xs !!? floor pOdd)
+  else mean (xs !!? ceiling pEven, xs !!? floor pEven)
+  where
+    pOdd = (3*(genericLength xs + 1) / 4) - 1
+    pEven = ((3 * genericLength xs + 2) / 4) - 1
+    mean (Just x, Just y) = Just ((x + y) / 2)
+    mean (Nothing, _) = Nothing
+    mean (_, Nothing) = Nothing
 
 
 median :: (Ord a, Fractional a) => [a] -> Maybe a
 median (sort -> xs) = if odd listSize
-  then xs !!? (listSize `div` 2)
+  then sort xs !!? (listSize `div` 2)
   else liftA2 (\x y -> (x + y) / 2)
-              (xs !!? (listSize `div` 2))
-              (xs !!? ((listSize `div` 2) - 1))
+              (sort xs !!? (listSize `div` 2))
+              (sort xs !!? ((listSize `div` 2) - 1))
   where listSize = length xs
 
 -- | calculates the superior and inferior discrepant values given
@@ -83,5 +100,5 @@ discrepants (_, q1, _, q3, _) = (liftA2 (-) q1 aq, liftA2 (+) q3 aq)
 cleanDiscrepants :: (Ord a, Fractional a) => [a] -> [a]
 cleanDiscrepants xs = if isNothing ci || isNothing cs
   then error "what the fuck"
-  else filter (\x -> not $ x < fromJust ci || x > fromJust cs) xs
+  else filter (\x ->  x >= fromJust ci && x <= fromJust cs) xs
   where (ci, cs) = discrepants $ fiveNumbers xs
