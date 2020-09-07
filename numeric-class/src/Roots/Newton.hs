8e133b89dc09a6e38ec40c098ea3b32fcedaf9a2
aiwaverse <aiwavision@protonmail.com>
2020-09-07 14:53:36 -0300

    added comments to newton

--- numeric-class/src/Roots/Newton.hs
+++ numeric-class/src/Roots/Newton.hs
@@ -3,6 +3,8 @@ module Roots.Newton
   )
 where
 
+-- | given @f@ and it's @derivative@, plus an @estimate@ and an @iteractions@ number
+-- calculates a root of @f@
 newton :: (Double -> Double) -> (Double -> Double) -> Double -> Int -> Double
 newton _ _ estimate 0 = estimate
 newton f derivative estimate iteractions =
