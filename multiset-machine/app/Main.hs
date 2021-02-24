946e00d3a81a769dc0119570e9cdb09e2fe9b4c5
aiwaverse <aiwavision@protonmail.com>
2021-02-24 12:44:37 -0300

    writing multiset

--- multiset-machine/app/Main.hs
+++ multiset-machine/app/Main.hs
@@ -1,7 +1,33 @@
 module Main (main) where
 
-import MultisetMachine (projectName)
-
+import MultisetMachine
 
 main :: IO ()
-main = putStrLn ("Executable for " ++ projectName)
+main = do
+  input <- getLine
+  let register = map (\c -> read @Int $ [c]) input
+  putStrLn $ printBitMultiset . program . fromList $ register
+
+registerS :: BitMultiset
+registerS = fromList [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+ins1 :: BitMultiset -> BitMultiset
+ins1 s = ins2 $ deleteBit Zero s
+
+ins2 :: BitMultiset -> BitMultiset
+ins2 s = if onlyOnes s then ins3 s else ins1 s
+
+ins3 :: BitMultiset -> BitMultiset
+ins3 s = ins4 $ deleteBit Zero . deleteBit One $ s
+
+ins4 :: BitMultiset -> BitMultiset
+ins4 s = if onlyOnes s then ins3 s else s
+
+program :: BitMultiset -> BitMultiset
+program s
+  | nZero > nOne = insereBit Zero $ ins1 s
+  | nZero < nOne = insereBit One $ ins1 s
+  | otherwise = ins1 s
+  where
+    nZero = length . filter (== Zero) $ s
+    nOne = length . filter (== One) $ s
--- multiset-machine/src/MultisetMachine.hs
+++ multiset-machine/src/MultisetMachine.hs
@@ -1,37 +1,41 @@
-{- |
-Copyright: (c) 2021 aiwa
-SPDX-License-Identifier: MIT
-Maintainer: aiwa <aiwavision@protonmail.com>
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE ViewPatterns #-}
 
-See README for more info
--}
-
-{-#LANGUAGE ViewPatterns#-}
-
+-- |
+-- Copyright: (c) 2021 aiwa
+-- SPDX-License-Identifier: MIT
+-- Maintainer: aiwa <aiwavision@protonmail.com>
+--
+-- See README for more info
 module MultisetMachine
-    ( printBitMultiset
-    , Bit
-    , BitMultiset
-    , printBit
-    , zeroGTOne
-    , oneGTZero
-    , onlyOnes
-    , onlyZeros
-    , insereBit
-    , deleteBit
-    ) where
+  ( printBitMultiset,
+    Bit (..),
+    BitMultiset,
+    printBit,
+    zeroGTOne,
+    oneGTZero,
+    onlyOnes,
+    onlyZeros,
+    insereBit,
+    deleteBit,
+    fromList,
+  )
+where
 
-import Data.List (foldl', delete)
+import Data.List (delete, foldl')
 
-projectName :: String
-projectName = "multiset-machine"
-
-data Bit = One | Zero deriving (Eq, Ord)
+data Bit = One | Zero deriving stock (Eq, Ord)
 
 type BitMultiset = [Bit]
 
 printBitMultiset :: BitMultiset -> String
 printBitMultiset = concatMap printBit
+
+fromList :: [Int] -> BitMultiset
+fromList [] = []
+fromList (0 : xs) = Zero : fromList xs
+fromList (1 : xs) = One : fromList xs
+fromList _ = error "Wrong numbers!"
 
 printBit :: Bit -> String
 printBit One = "1"
@@ -44,20 +48,20 @@ ones :: BitMultiset -> Int
 ones = length . filter (== One)
 
 zeroGTOne :: BitMultiset -> Bool
-zeroGTOne s = zeros s > ones s 
+zeroGTOne s = zeros s > ones s
 
 oneGTZero :: BitMultiset -> Bool
 oneGTZero s = ones s > zeros s
 
 onlyZeros :: BitMultiset -> Bool
 onlyZeros s = o == (0 :: Int) && z >= (1 :: Int)
-    where
-        (z, o) = foldl' (\(x, y) l -> if l == Zero then (x + 1, y) else (x, y + 1)) (0, 0) s
+  where
+    (z, o) = foldl' (\(x, y) l -> if l == Zero then (x + 1, y) else (x, y + 1)) (0, 0) s
 
 onlyOnes :: BitMultiset -> Bool
 onlyOnes s = z == (0 :: Int) && o >= (1 :: Int)
-    where
-        (z, o) = foldl' (\(x, y) l -> if l == Zero then (x + 1, y) else (x, y + 1)) (0, 0) s
+  where
+    (z, o) = foldl' (\(x, y) l -> if l == Zero then (x + 1, y) else (x, y + 1)) (0, 0) s
 
 invert :: Bit -> Bit
 invert One = Zero
@@ -65,12 +69,12 @@ invert Zero = One
 
 insereBit :: Bit -> BitMultiset -> BitMultiset
 insereBit b = invertOrNot . (b :)
-    where
-        otherBitCount = if b == Zero then ones else zeros
-        invertOrNot xs = if length xs > otherBitCount xs then map invert xs else xs 
+  where
+    otherBitCount = if b == Zero then ones else zeros
+    invertOrNot xs = if length xs > otherBitCount xs then map invert xs else xs
 
 deleteBit :: Bit -> BitMultiset -> BitMultiset
 deleteBit b = invertOrNot . delete b
-    where
-        otherBitCount = if b == Zero then ones else zeros
-        invertOrNot xs = if length xs > otherBitCount xs then map invert xs else xs 
+  where
+    otherBitCount = if b == Zero then ones else zeros
+    invertOrNot xs = if length xs > otherBitCount xs then map invert xs else xs
